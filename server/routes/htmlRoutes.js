511bfe3b64a1fe710555d457ff671a02e6f6c69c
Gordo <gordonhdo@gmail.com>
2022-05-05 13:39:34 -0700

    added comments to html routes

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,7 @@
+// import path module
 const path = require('path');
 
+// export path to access client via index.html
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
