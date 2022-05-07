084a4f839119b4dc7c89f746604ab53b67e6f80e
Gordo <gordonhdo@gmail.com>
2022-05-06 18:20:19 -0700

    minor changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,6 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.log('PUT to database');
   const jateDb = await openDB('jate', 1);
@@ -24,6 +25,7 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.log('GET from database');
   const jateDb = await openDB('jate', 1);
